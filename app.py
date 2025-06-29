06c86e100569e8eccfdeb03ac8c7e854324caca0
deeptechturkiye <deeptechturkiye@gmail.com>
2025-06-29 07:05:47 +0300

    Render uyumlu PORT ayarı eklendi

--- app.py
+++ app.py
@@ -118,4 +118,5 @@ def webhook():
         return jsonify({"message": str(e), "status": "error"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
